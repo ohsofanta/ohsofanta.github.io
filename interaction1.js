07c2aa8cc4fb63e852a72585a730767bbb0e9483
yanwenwang <yanwenwang1125@gmail.com>
2024-03-21 12:38:42 -0700

    more update

--- interaction1.js
+++ interaction1.js
@@ -19,3 +19,95 @@ buttons.forEach((button, index) => {
         }
     });
 });
+
+
+
+// Adding an event listener to the button to toggle play/pause for each song
+document.getElementById('playButton1').addEventListener('click', function() {
+    // Getting the audio elements for each song
+    const audio1 = document.getElementById('audio1');
+    const audio2 = document.getElementById('audio5');
+    const audio3 = document.getElementById('audio3');
+
+    // Toggling play/pause for the first song
+    if (audio1.paused) {
+        audio1.play();
+    } else {
+        audio1.pause();
+    }
+
+    // Toggling play/pause for the second song
+    if (audio2.paused) {
+        audio2.play();
+    } else {
+        audio2.pause();
+    }
+
+    // Toggling play/pause for the third song
+    if (audio3.paused) {
+        audio3.play();
+    } else {
+        audio3.pause();
+    }
+});
+
+
+// Adding an event listener to the button to toggle play/pause for each song
+document.getElementById('playButton2').addEventListener('click', function() {
+    // Getting the audio elements for each song
+    const audio1 = document.getElementById('audio4');
+    const audio2 = document.getElementById('audio2');
+    const audio3 = document.getElementById('audio9');
+
+    // Toggling play/pause for the first song
+    if (audio1.paused) {
+        audio1.play();
+    } else {
+        audio1.pause();
+    }
+
+    // Toggling play/pause for the second song
+    if (audio2.paused) {
+        audio2.play();
+    } else {
+        audio2.pause();
+    }
+
+    // Toggling play/pause for the third song
+    if (audio3.paused) {
+        audio3.play();
+    } else {
+        audio3.pause();
+    }
+});
+
+// Adding an event listener to the button to toggle play/pause for each song
+document.getElementById('playButton3').addEventListener('click', function() {
+    // Getting the audio elements for each song
+    const audio1 = document.getElementById('audio7');
+    const audio2 = document.getElementById('audio8');
+    const audio3 = document.getElementById('audio6');
+
+    // Toggling play/pause for the first song
+    if (audio1.paused) {
+        audio1.play();
+    } else {
+        audio1.pause();
+    }
+
+    // Toggling play/pause for the second song
+    if (audio2.paused) {
+        audio2.play();
+    } else {
+        audio2.pause();
+    }
+
+    // Toggling play/pause for the third song
+    if (audio3.paused) {
+        audio3.play();
+    } else {
+        audio3.pause();
+    }
+});
+
+
